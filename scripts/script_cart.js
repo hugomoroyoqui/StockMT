f2e401775cd5beb39137f3dd12a87386cd4cac2a
Alex <alex1071756@gmail.com>
2018-05-18 01:31:55 -0700

    Cart fixes

--- scripts/script_cart.js
+++ scripts/script_cart.js
@@ -1,34 +1,36 @@
 $(document).ready(function(){
   var cart = JSON.parse(window.localStorage.getItem("cart"));
-  console.log(cart);
-  var i = 0;
-  if(cart){
+  if(cart && cart.length > 0){
+    var i = 0;
     cart.forEach(function(product){
       appendProduct(i, product);
       i++;
     });
     $('.materialboxed').materialbox();
+    $(".progress").hide(200);
+  } else {
+    $(".progress").hide(200);
   }
-  $(".progress").hide(200);
+
 })
 
 function appendProduct(id, product) {
   $("#cart_wrapper").append(`
     <li class="collection-item avatar home-product">
-      <div class="col s12 m6">
-        <img src="` + Utils.imgThumb(product.picture) + `" height="80" class="materialboxed">
-        <span class="description">` + product.description + `</span>
-        <p class="part_number">` + Utils.priceText(product.cost) +  `</p>
-      </div>
-      <div class="col s2">
-        <p class="center-align">` + product.part_number + `<br>` + product.category + `</p>
-      </div>
-      <div class="col m1 hide-on-small"></div>
-      <div class="col s12 m3">
+    <div class="col s12 m6">
+    <img src="` + Utils.imgThumb(product.picture) + `" height="80" class="materialboxed">
+    <span class="description">` + product.description + `</span>
+    <p class="part_number">` + Utils.priceText(product.cost) +  `</p>
+    </div>
+    <div class="col s2">
+    <p class="center-align">` + product.part_number + `<br>` + product.category + `</p>
+    </div>
+    <div class="col m1 hide-on-small"></div>
+    <div class="col s12 m3">
 
-      </div>
-      <span class="col s2">` + product.quantity + ` requested</span>
+    </div>
+    <span class="col s2">` + product.quantity + ` requested</span>
 
     </li>
-  `);
-}
+    `);
+  }
