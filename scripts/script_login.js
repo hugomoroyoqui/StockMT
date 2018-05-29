279b0b05911a8fda6a1faf401cdda9334c21fd0c
Alex <alex1071756@gmail.com>
2018-05-29 02:58:05 -0700

    Toolcrib 2

--- scripts/script_login.js
+++ scripts/script_login.js
@@ -1,8 +1,62 @@
 document.body.style.backgroundImage = "url('res/img/research2.png')";
 
 $(document).ready(function(){
-  loadReadyOrders();
+  loadOrders();
 })
+
+function loadOrders() {
+  var ref = firebase.database().ref('Orders').orderByChild('status').equalTo('ready');
+  ref.on('value', function(snapshot) {
+      $("#ready_to_pick_up").empty();
+      if(snapshot.numChildren() > 0){
+        snapshot.forEach(function(childSnapshot) {
+          appendCard(childSnapshot);
+        });
+      } else {
+        showNoOrders();
+      }
+
+})
+}
+
+function showNoOrders() {
+  $("#ready_to_pick_up").append(`
+    <div class="card grey lighten-4" style="display: none;">
+      <div class="card-content black-text">
+        <span class="items_title"><i class="material-icons left">update</i>No ready orders yet!</span>
+      </div>
+    </div>
+    `);
+    $(".card").show(200);
+}
+
+function appendCard(order) {
+  $("#ready_to_pick_up").append(`
+    <div class="card blue darken-2" style="display: none;">
+      <div class="card-content white-text">
+        <span class="items_title"><b>` + generateDescription(order) + `</b></span>
+        <p></p>
+        <p>` + timeago().format(order.val().time) + `</p>
+      </div>
+      <div class="card-action white-text">
+        <span>To: Alejandro Ríos: 345351</span>
+      </div>
+    </div>
+    `);
+    $(".card").show(200);
+    showNotification();
+}
+
+function generateDescription(order) {
+  var description = order.val().products[0].quantity + " " + order.val().products[0].description;
+  if(order.val().products.length > 1) {
+    for (var i = 1; i < order.val().products.length; i++) {
+    description = description + ", " + order.val().products[i].quantity + " " + order.val().products[i].description
+
+  }
+}
+return description;
+}
 
 function loadReadyOrders() {
   var orders = firebase.database().ref('Orders').orderByChild('status').equalTo('ready');
